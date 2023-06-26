b3de57cbd797068ba02deaf5e5aeaa59c200b10f
Mark Prodehl <markprodehl@gmail.com>
2023-06-26 09:53:52 -0700

    Added a host vite.config to for hosting purposes as well as to allow mobile access to localhost:8080

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Add for Kinsta Hosting and to allow your mobile access on localhost:8080
     port: 8080   // set your desired port here
   }
 })
